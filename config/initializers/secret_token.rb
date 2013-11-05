35fce9b4e3b411c5c7b3426bd037bbbaa6ff8bdc
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-11-05 00:03:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContractorApp::Application.config.secret_token = '29498576b2a7b592e156598a7a3051195ea7ee852638538f5c4f5d80916266bb618dbb78fc3495266ef21dc3a05b520c3bf1be8babb19cfda04c209fe2981e52'
