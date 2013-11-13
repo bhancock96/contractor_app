0840bb14ebc42d66b1074ab8c8eee36ee08aa6cf
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-11-13 14:05:22 -0600

    change session route format to fix routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 ContractorApp::Application.routes.draw do
-  resources :sessions
-  resources :contractors
+  resources :contractors do
+  	resources :jobs
+  end
+  
+  post '/sessions', :to => 'sessions#create', :as => 'create_session'
+  get '/sessions/new', :to => 'sessions#new', :as => 'new_session'
+  post '/logout', :to => 'sessions#destroy', :as => 'logout'
 
   root :to => 'welcome#index'
 end
