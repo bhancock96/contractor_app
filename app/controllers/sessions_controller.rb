2a96ed29f2345d14076b85b26772180b0332b8a0
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-12-01 18:21:34 -0600

    changed indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,18 @@ class SessionsController < ApplicationController
 
 	def create
 		contractor = Contractor.find_by_email(params[:email])
-  	if contractor && contractor.authenticate(params[:password])
-    	session[:contractor_id] = contractor.id
-    	redirect_to contractor_url(contractor)
-  	else
-    	flash[:notice] = "Invalid username or password."
-    	render new_session_url
-  	end
+	  	if contractor && contractor.authenticate(params[:password])
+	    	session[:contractor_id] = contractor.id
+	    	flash[:notice] = "Successfully logged in"
+	    	redirect_to contractor_url(contractor)
+	  	else
+	    	flash[:notice] = "Invalid username or password."
+	    	render new_session_url
+	  	end
 	end
 
 	def destroy
 		reset_session
-    redirect_to new_session_url, :notice => "You've successfully logged out"
+    	redirect_to new_session_url, :notice => "You've successfully logged out"
 	end
 end
