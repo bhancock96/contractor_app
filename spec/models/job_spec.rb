1d1c52962bb730783385e12cc54c97cb39b1fdf2
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-12-10 00:51:59 -0600

    added image association to job test

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Job do
   it { should belong_to(:contractor) }
   it { should have_many(:expenses) }
+  it { should have_many(:images)}
 
   it { should validate_presence_of(:customer_name) }
   it { should validate_presence_of(:customer_street_address) }
