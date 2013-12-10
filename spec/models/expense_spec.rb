d617afa1cd19648032a2641166e5d1760ef06e12
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-12-10 00:50:52 -0600

    test for job_expenses method

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Expense do
   it { should belong_to(:job)}
+  it { should belong_to(:contractor)}
 
   it { should validate_presence_of(:store) }
   it { should validate_presence_of(:expense_type) }
@@ -10,4 +11,10 @@ describe Expense do
   it { should allow_mass_assignment_of(:store)}
   it { should allow_mass_assignment_of(:expense_type)}
   it { should allow_mass_assignment_of(:amount)}
+
+  it "should create an array of all job expenses" do
+  	@job = FactoryGirl.build_stubbed(:job)
+  	@expenses == 4.times{ FactoryGirl.create(:expense, job_id: @job.id) }
+  	@job.job_expenses.should eq([21.21, 21.21, 21.21, 21.21])
+  end
 end
