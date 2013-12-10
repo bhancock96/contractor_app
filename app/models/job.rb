07bdae53d47e41c65c75a188144b7b6f93aa8461
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-12-10 00:47:58 -0600

    added methods to select each type of image

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,8 +8,34 @@ class Job < ActiveRecord::Base
 
   belongs_to :contractor
   has_many :expenses
+  has_many :images
 
   validates_presence_of :customer_name, :customer_street_address,
   						:customer_zipcode, :customer_phone_number,
   						:customer_email, :customer_city
+
+
+  def job_expenses
+  	expense_amounts = []
+  	self.expenses.each do |expense|
+  		expense_amounts << expense.amount.to_f.round(2)
+  	end
+  	expense_amounts
+  end
+
+  def before_images
+  	self.images.where(image_type: 'Before')
+  end
+
+  def after_images
+  	self.images.where(image_type: 'After')
+  end
+
+  def proposal_images
+    self.images.where(image_type: 'Proposal')
+  end
+
+  def contract_images
+    self.images.where(image_type: 'Contract')
+  end
 end
