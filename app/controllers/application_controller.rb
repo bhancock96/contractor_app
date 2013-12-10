2608ec71b2457029e5d7016b237bc4bad4636d95
Bryan <bryan@Bryans-MacBook-Pro.local>
2013-12-10 00:35:27 -0600

    added current_contractor helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_contractor
+  
+  def current_contractor
+  	@current_contractor || Contractor.find(session[:contractor_id])
+  end
 end
