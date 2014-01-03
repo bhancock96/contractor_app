580c1ccf7ac4b1458f2bb532316acfb1a8f8546b
Bryan <bryan@Bryans-MacBook-Pro.local>
2014-01-03 13:09:56 -0600

    added new styling

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,17 @@
 class EmployeesController < ApplicationController
+	def new
+		@employee = Employee.new
+	end
+
+	def create
+		contractor = Contractor.find(params[:contractor_id])
+		employee = contractor.employees.create(params[:employee])
+		if employee.save
+			redirect_to contractor_employee_url(contractor, employee)
+			flash[:notice] = "Employee successfully created"
+		else
+			render new_contractor_employee_url(contractor)
+			flash[:notice] = "Name, phone number, birth date, and date hired must all be filled in."
+		end
+	end
 end
